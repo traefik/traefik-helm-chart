fb5215c8b41f391c5f0997573ffcebf07d49b55d
Rémi BUISSON <remi.buisson@traefik.io>
2024-10-18 12:05:32 +0200

    fix: changelog

--- hack/changelog.sh
+++ hack/changelog.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-chart="./traefik"
-
-version="$(awk '/^version:/ { print $2} ' ${chart}/Chart.yaml)"
-changelog="$(sed -e "1,/^## ${version}/d" -e "/^###/,\$d" -e '/^$/d' -e 's/^* /- /' -e 's/^/    /' ${chart}/Changelog.md | grep '^    - ' | sed -e 's/\ *$//g' | sed 's/    - \(.*\)/    - "\1"/g')"
-sed -i -e '/^  artifacthub.io\/changes: |/,$d' ${chart}/Chart.yaml
-echo "  artifacthub.io/changes: |" >> ${chart}/Chart.yaml
-echo "${changelog}" >> ${chart}/Chart.yaml
+for chart in "./traefik" "./traefik-crds"; do
+  version="$(awk '/^version:/ { print $2} ' ${chart}/Chart.yaml)"
+  changelog="$(sed -e "1,/^## ${version}/d" -e "/^###/,\$d" -e '/^$/d' -e 's/^* /- /' -e 's/^/    /' ${chart}/Changelog.md | grep '^    - ' | sed -e 's/\ *$//g' | sed 's/    - \(.*\)/    - "\1"/g')"
+  sed -i -e '/^  artifacthub.io\/changes: |/,$d' ${chart}/Chart.yaml
+  echo "  artifacthub.io/changes: |" >>${chart}/Chart.yaml
+  echo "${changelog}" >>${chart}/Chart.yaml
+done
