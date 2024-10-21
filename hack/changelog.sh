5f39c43cc9ebe01e9aa02ecfc6ce86513165b72f
Rémi BUISSON <remi.buisson@traefik.io>
2024-10-21 16:12:02 +0200

    fix: reduce changelog on patches

--- hack/changelog.sh
+++ hack/changelog.sh
@@ -2,7 +2,7 @@
 
 for chart in "./traefik" "./traefik-crds"; do
   version="$(awk '/^version:/ { print $2} ' ${chart}/Chart.yaml)"
-  changelog="$(sed -e "1,/^## ${version}/d" -e "/^###/,\$d" -e '/^$/d' -e 's/^* /- /' -e 's/^/    /' ${chart}/Changelog.md | grep '^    - ' | sed -e 's/\ *$//g' | sed 's/    - \(.*\)/    - "\1"/g')"
+  changelog="$(sed -e "1,/^## ${version}/d" -e "/^##/,\$d" -e '/^$/d' -e 's/^* /- /' -e 's/^/    /' ${chart}/Changelog.md | grep '^    - ' | sed -e 's/\ *$//g' | sed 's/    - \(.*\)/    - "\1"/g')"
   sed -i -e '/^  artifacthub.io\/changes: |/,$d' ${chart}/Chart.yaml
   echo "  artifacthub.io/changes: |" >>${chart}/Chart.yaml
   echo "${changelog}" >>${chart}/Chart.yaml
