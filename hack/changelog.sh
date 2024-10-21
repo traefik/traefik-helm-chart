0ac444ecf345c02f629f7f3114e132bed76e4d52
Rémi BUISSON <remi.buisson@traefik.io>
2024-10-21 17:09:22 +0200

    fix: more reliable changelog.sh

--- hack/changelog.sh
+++ hack/changelog.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 for chart in "./traefik" "./traefik-crds"; do
-  version="$(awk '/^version:/ { print $2} ' ${chart}/Chart.yaml)"
+  version=$(yq -r '.version' <"${chart}/Chart.yaml")
   changelog="$(sed -e "1,/^## ${version}/d" -e "/^##/,\$d" -e '/^$/d' -e 's/^* /- /' -e 's/^/    /' ${chart}/Changelog.md | grep '^    - ' | sed -e 's/\ *$//g' | sed 's/    - \(.*\)/    - "\1"/g')"
+
+  echo "${version}"
+  echo "${changelog}"
+
+  sed -i -r 's/^annotations: \{\}/annotations:/g' ${chart}/Chart.yaml
   sed -i -e '/^  artifacthub.io\/changes: |/,$d' ${chart}/Chart.yaml
   echo "  artifacthub.io/changes: |" >>${chart}/Chart.yaml
   echo "${changelog}" >>${chart}/Chart.yaml
