b6bc446c8d105a7920d021ace5b9a9190cc96287
Rémi BUISSON <remi.buisson@traefik.io>
2025-01-10 09:31:04 +0100

    fix: CI

--- hack/ct.sh
+++ hack/ct.sh
@@ -7,8 +7,9 @@ git config --global --add safe.directory /charts
 case "${ACTION}" in
 install-with-crds)
   ACTION="install"
-  ct "${ACTION}" --config=.github/chart-testing.yaml --charts traefik-crds/
-  ct "${ACTION}" --config=.github/chart-testing.yaml --charts traefik/  --helm-extra-args '--skip-crds'
+  ct "${ACTION}" --config=.github/chart-testing.yaml --charts traefik-crds/ --skip-clean-up
+  # # --skip-clean-up is here because helm uninstall doesn't have a --skip-crds flag ...
+  ct "${ACTION}" --config=.github/chart-testing.yaml --charts traefik/ --helm-extra-args '--skip-crds' --skip-clean-up
   ;;
 install)
   ct "${ACTION}" --config=.github/chart-testing.yaml --charts traefik/
