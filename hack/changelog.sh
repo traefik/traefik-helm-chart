27edbf4106860c9656b7b997e0ce131da0495e2b
Rémi BUISSON <remi.buisson@traefik.io>
2026-03-27 14:09:47 +0100

    fix(release): :rocket: use latest helm-changelog + fix changelog script

--- hack/changelog.sh
+++ hack/changelog.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if ! sed --version 2>/dev/null | grep -q "GNU sed"; then
+  echo "Error: GNU sed is required. On macOS, install it with: brew install gnu-sed"
+  echo "Then either add it to your PATH or use: PATH=\"\$(brew --prefix gnu-sed)/libexec/gnubin:\$PATH\" $0"
+  exit 1
+fi
+
 for chart in "./traefik" "./traefik-crds"; do
   version=$(yq -r '.version' <"${chart}/Chart.yaml")
   changelog="$(sed -e "1,/^## ${version}/d" -e "/^##/,\$d" -e '/^$/d' -e 's/^* /- /' -e 's/^/    /' ${chart}/Changelog.md | grep '^    - ' | sed -e 's/\ *$//g' | sed 's/    - \(.*\)/    - "\1"/g')"
