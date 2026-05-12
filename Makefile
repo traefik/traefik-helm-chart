fd6fca3273a4e452b7fdf78e98611621630215fb
Rémi BUISSON <remi.buisson@traefik.io>
2026-05-12 09:43:27 +0200

    chore(crds): :wastebasket: remove deprecated traefik-crds chart

--- Makefile
+++ Makefile
@@ -6,16 +6,12 @@ IMAGE_HELM_DOCS=jnorwood/helm-docs:v1.14.2
 
 traefik/tests/__snapshot__:
 	@mkdir traefik/tests/__snapshot__
-	@mkdir traefik-crds/tests/__snapshot__
 
 test: traefik/tests/__snapshot__
 	./hack/test.sh
 
 test-ns:
 	./hack/check-ns.sh
-
-test-crds-consistency:
-	./hack/check-crds-consistency.sh
 
 lint:
 	docker run ${DOCKER_ARGS} --env GIT_SAFE_DIR="true" --entrypoint /bin/sh --rm -v $(CURDIR):/charts -w /charts $(IMAGE_CHART_TESTING) /charts/hack/ct.sh lint
@@ -32,7 +28,6 @@ test-%:
 # $ helm plugin install https://github.com/losisin/helm-values-schema-json.git
 schema:
 	cd traefik && helm schema --use-helm-docs
-	cd traefik-crds && helm schema
 
 changelog:
 	@echo "== Updating Changelogs..."
--- hack/changelog.sh
+++ hack/changelog.sh
@@ -6,15 +6,14 @@ if ! sed --version 2>/dev/null | grep -q "GNU sed"; then
   exit 1
 fi
 
-for chart in "./traefik" "./traefik-crds"; do
-  version=$(yq -r '.version' <"${chart}/Chart.yaml")
-  changelog="$(sed -e "1,/^## ${version}/d" -e "/^##/,\$d" -e '/^$/d' -e 's/^* /- /' -e 's/^/    /' ${chart}/Changelog.md | grep '^    - ' | sed -e 's/\ *$//g' | sed 's/    - \(.*\)/    - "\1"/g')"
+chart="./traefik"
+version=$(yq -r '.version' <"${chart}/Chart.yaml")
+changelog="$(sed -e "1,/^## ${version}/d" -e "/^##/,\$d" -e '/^$/d' -e 's/^* /- /' -e 's/^/    /' ${chart}/Changelog.md | grep '^    - ' | sed -e 's/\ *$//g' | sed 's/    - \(.*\)/    - "\1"/g')"
 
-  echo "${version}"
-  echo "${changelog}"
+echo "${version}"
+echo "${changelog}"
 
-  sed -i -r 's/^annotations: \{\}/annotations:/g' ${chart}/Chart.yaml
-  sed -i -e '/^  artifacthub.io\/changes:/,$d' ${chart}/Chart.yaml
-  echo "  artifacthub.io/changes: |" >>${chart}/Chart.yaml
-  echo "${changelog}" >>${chart}/Chart.yaml
-done
+sed -i -r 's/^annotations: \{\}/annotations:/g' ${chart}/Chart.yaml
+sed -i -e '/^  artifacthub.io\/changes:/,$d' ${chart}/Chart.yaml
+echo "  artifacthub.io/changes: |" >>${chart}/Chart.yaml
+echo "${changelog}" >>${chart}/Chart.yaml
