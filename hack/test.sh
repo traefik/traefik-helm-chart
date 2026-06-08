0a6e50fcc9e0cc0542d5fa5dd033651cf13e6f77
Rémi BUISSON <remi.buisson@traefik.io>
2026-06-08 16:03:17 +0200

    ci(test): fix helm-unittest invocation for helm v4 and bump plugin to 1.1.0

--- hack/test.sh
+++ hack/test.sh
@@ -9,8 +9,11 @@ fi
 
 if ! helm plugin list 2>/dev/null | awk 'NR>1 && $1=="unittest"{found=1} END{exit !found}'; then
   echo "Error: helm-unittest plugin is not installed." >&2
-  echo "Install: helm plugin install https://github.com/helm-unittest/helm-unittest --version 1.0.1" >&2
+  echo "Install: helm plugin install https://github.com/helm-unittest/helm-unittest --version 1.1.0" >&2
   exit 1
 fi
 
-helm unittest --color ./traefik
+# NB: --color is intentionally omitted. Helm v4 introduced a global --color <mode>
+# persistent flag that shadows the helm-unittest boolean --color flag, breaking the
+# invocation. Color is auto-detected from the tty instead.
+helm unittest ./traefik
